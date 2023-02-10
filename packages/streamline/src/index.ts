e43a43b0162dd3e9879a5dedde64d88407b91ffd
bastienluko <bastien@getluko.com>
2023-02-10 11:09:19 +0100

    Fix imports

--- packages/streamline/src/index.ts
+++ packages/streamline/src/index.ts
@@ -3,3 +3,4 @@ export * from './primitives/text/text';
 export * from './primitives/icon/icon';
 export * from './theme';
 export * from './theme/ThemeProvider';
+export * as GeneratedRegularIcons from './primitives/icon/generated/regular';

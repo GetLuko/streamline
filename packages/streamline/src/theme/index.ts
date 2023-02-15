ee8314600580bb3a59c56c36f3391fbf3e35ae06
Mickael Dumand <mickael@getluko.com>
2023-02-15 11:36:18 +0100

    Streamline fix: format with prettier

--- packages/streamline/src/theme/index.ts
+++ packages/streamline/src/theme/index.ts
@@ -1,30 +1,30 @@
-import { createTheme, useTheme } from "@shopify/restyle";
+import { createTheme, useTheme } from '@shopify/restyle';
 
-import { colors } from "./colors";
-import { radius } from "./radius";
-import { spacings } from "./spacings";
-import { fonts } from "./fonts";
-export { fonts, TextVariants as TextVariant}  from "./fonts";
+import { colors } from './colors';
+import { radius } from './radius';
+import { spacings } from './spacings';
+import { fonts } from './fonts';
+export { fonts, TextVariants as TextVariant } from './fonts';
 
 export const theme = createTheme({
-    textVariants: fonts,
-    colors: {
-        ...colors.PRIMARY,
-        ...colors.GREY,
-        ...colors.PURPLE,
-        ...colors.TEAL,
-        ...colors.BLUKO,
-        ...colors.ORANGE,
-        ...colors.MINT,
-        ...colors.GOLD,
-        ...colors.TERRA,
-    },
-    spacing: spacings,
-    breakpoints: {
-        phone: 0,
-    },
-    borderRadii: radius
-})
+  textVariants: fonts,
+  colors: {
+    ...colors.PRIMARY,
+    ...colors.GREY,
+    ...colors.PURPLE,
+    ...colors.TEAL,
+    ...colors.BLUKO,
+    ...colors.ORANGE,
+    ...colors.MINT,
+    ...colors.GOLD,
+    ...colors.TERRA,
+  },
+  spacing: spacings,
+  breakpoints: {
+    phone: 0,
+  },
+  borderRadii: radius,
+});
 
 export type ColorTheme = keyof Theme['colors'];
 export type Theme = typeof theme;

43d38637e9c2bb02e83ac67c5c317f3d3b354654
bastienluko <bastien@getluko.com>
2023-02-23 12:12:14 +0100

    Fix Sandbox

--- packages/sandbox/src/app/sandbox/docs/spinner.doc.tsx
+++ packages/sandbox/src/app/sandbox/docs/spinner.doc.tsx
@@ -1,11 +1,11 @@
-import { Spinner } from '@getluko/streamline';
+import { Box, Spinner } from '@getluko/streamline';
 
 export const SpinnerSandbox = () => (
-  <>
+  <Box flex={1} alignItems="flex-start">
     <Spinner />
     <Spinner color="TERRA_500" />
     <Spinner size="small" />
     <Spinner size="regular" />
     <Spinner size="large" />
-  </>
+  </Box>
 );

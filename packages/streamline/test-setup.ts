4af4f71d2a562924413120cba745f5ee69d265f7
bastienluko <bastien@getluko.com>
2023-02-27 14:55:05 +0100

    Fix Publish Action

--- packages/streamline/test-setup.ts
+++ packages/streamline/test-setup.ts
@@ -1,7 +1,1 @@
 import '@testing-library/jest-native/extend-expect';
-
-jest.mock('react-native-ama/dist/commonjs/internal/logger.js', () => {
-  return {
-    getContrastCheckerMaxDepth: () => 5,
-  };
-});

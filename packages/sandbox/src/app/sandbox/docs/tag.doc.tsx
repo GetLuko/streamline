8e49c5d864da9410c4cb73fcba6e328aca50b6e9
Théo Judas <theo@getluko.com>
2023-03-15 09:57:58 +0100

    Change Sandbox tag description

--- packages/sandbox/src/app/sandbox/docs/tag.doc.tsx
+++ packages/sandbox/src/app/sandbox/docs/tag.doc.tsx
@@ -1,48 +1,52 @@
-import { Box, Button, Tag } from '@getluko/streamline';
+import { Box, Tag } from '@getluko/streamline';
 
-const TEXT = 'Tag';
 const ICON_NAME = 'Area';
 
 export const TagSandbox = () => (
   <Box paddingHorizontal="xl">
     <Box paddingVertical="xxs" alignItems="flex-end" flexDirection="row">
       <Box padding="xxs">
-        <Tag appearance="primary" iconName={ICON_NAME} text={TEXT} />
+        <Tag appearance="primary" iconName={ICON_NAME} text="Primary" />
       </Box>
       <Box padding="xxs">
-        <Tag appearance="primary" isSmall iconName={ICON_NAME} text={TEXT} />
-      </Box>
-    </Box>
-    <Box paddingVertical="xxs" alignItems="flex-end" flexDirection="row">
-      <Box padding="xxs">
-        <Tag appearance="secondary" iconName={ICON_NAME} text={TEXT} />
-      </Box>
-      <Box padding="xxs">
-        <Tag appearance="secondary" isSmall iconName={ICON_NAME} text={TEXT} />
+        <Tag appearance="primary" isSmall iconName={ICON_NAME} text="Primary" />
       </Box>
     </Box>
     <Box paddingVertical="xxs" alignItems="flex-end" flexDirection="row">
       <Box padding="xxs">
-        <Tag appearance="success" iconName={ICON_NAME} text={TEXT} />
+        <Tag appearance="secondary" iconName={ICON_NAME} text="Secondary" />
       </Box>
       <Box padding="xxs">
-        <Tag appearance="success" isSmall iconName={ICON_NAME} text={TEXT} />
-      </Box>
-    </Box>
-    <Box paddingVertical="xxs" alignItems="flex-end" flexDirection="row">
-      <Box padding="xxs">
-        <Tag appearance="warning" iconName={ICON_NAME} text={TEXT} />
-      </Box>
-      <Box padding="xxs">
-        <Tag appearance="warning" isSmall iconName={ICON_NAME} text={TEXT} />
+        <Tag
+          appearance="secondary"
+          isSmall
+          iconName={ICON_NAME}
+          text="Secondary"
+        />
       </Box>
     </Box>
     <Box paddingVertical="xxs" alignItems="flex-end" flexDirection="row">
       <Box padding="xxs">
-        <Tag appearance="danger" iconName={ICON_NAME} text={TEXT} />
+        <Tag appearance="success" iconName={ICON_NAME} text="Success" />
       </Box>
       <Box padding="xxs">
-        <Tag appearance="danger" isSmall iconName={ICON_NAME} text={TEXT} />
+        <Tag appearance="success" isSmall iconName={ICON_NAME} text="Success" />
+      </Box>
+    </Box>
+    <Box paddingVertical="xxs" alignItems="flex-end" flexDirection="row">
+      <Box padding="xxs">
+        <Tag appearance="warning" iconName={ICON_NAME} text="Warning" />
+      </Box>
+      <Box padding="xxs">
+        <Tag appearance="warning" isSmall iconName={ICON_NAME} text="Warning" />
+      </Box>
+    </Box>
+    <Box paddingVertical="xxs" alignItems="flex-end" flexDirection="row">
+      <Box padding="xxs">
+        <Tag appearance="danger" iconName={ICON_NAME} text="Danger" />
+      </Box>
+      <Box padding="xxs">
+        <Tag appearance="danger" isSmall iconName={ICON_NAME} text="Danger" />
       </Box>
     </Box>
   </Box>

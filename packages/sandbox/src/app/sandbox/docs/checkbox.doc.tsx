bddc342bc91d0d06052ec518ec5c4ac46c6c95c1
Hugo Courthias <hugo.courthias@getluko.com>
2023-04-11 15:30:53 +0200

    refacto to bypass android issue + better radius

--- packages/sandbox/src/app/sandbox/docs/checkbox.doc.tsx
+++ packages/sandbox/src/app/sandbox/docs/checkbox.doc.tsx
@@ -1,15 +1,21 @@
-import { DocList } from '../components/DocList';
-import { Checkbox } from '@getluko/streamline';
+import { useState } from 'react';
+import { Box, Checkbox } from '@getluko/streamline';
 
-const docs: JSX.Element[] = [
-  <Checkbox value={false} />,
-  <Checkbox value={false} disabled />,
-  <Checkbox value={true} />,
-  <Checkbox value={true} disabled />,
-  <Checkbox appearance="danger" value={false} />,
-  <Checkbox appearance="danger" value={false} disabled />,
-  <Checkbox appearance="danger" value={true} />,
-  <Checkbox appearance="danger" value={true} disabled />,
-];
-
-export const CheckboxSandbox = () => <DocList docs={docs} />;
+export const CheckboxSandbox = () => {
+  const [checked, setChecked] = useState(false);
+  const [dangerChecked, setDangerChecked] = useState(false);
+  return (
+    <Box margin={'md'}>
+      <Checkbox value={checked} onChange={(v) => setChecked(v)} />
+      <Checkbox
+        appearance="danger"
+        value={dangerChecked}
+        onChange={(v) => setDangerChecked(v)}
+      />
+      <Checkbox value={true} disabled />
+      <Checkbox value={false} disabled />
+      <Checkbox appearance="danger" value={false} disabled />
+      <Checkbox appearance="danger" value={true} disabled />
+    </Box>
+  );
+};
--- packages/streamline/src/components/checkbox/checkbox.tsx
+++ packages/streamline/src/components/checkbox/checkbox.tsx
@@ -1,8 +1,9 @@
-import { Pressable, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet, ViewStyle } from 'react-native';
 import { CheckboxProps } from './checkbox.types';
 import { getCheckboxColors } from './checkbox.utils';
 import { Box } from '../../primitives/box/box';
 import { Icon } from '../../primitives/icon/icon';
+import { useStreamlineTheme } from '../../theme';
 
 export const Checkbox = ({
   appearance = 'primary',
@@ -12,6 +13,7 @@ export const Checkbox = ({
   testID,
 }: CheckboxProps) => {
   const colors = getCheckboxColors({ appearance, disabled });
+  const styles = useStyles();
 
   const handleOnPress = () => {
     onChange?.(!value);
@@ -20,53 +22,83 @@ export const Checkbox = ({
   return (
     <Pressable
       disabled={disabled}
-      style={styles.pressable}
       hitSlop={10}
       onPress={handleOnPress}
       testID={testID}
+      style={styles.pressable}
     >
       {({ pressed }) => (
-        <>
-          {pressed ? (
-            <Box
-              position={'absolute'}
-              top={0}
-              left={0}
-              right={0}
-              bottom={0}
-              backgroundColor={colors.shadowColor}
-              borderRadius={'sm'}
-            />
-          ) : null}
+        <Box
+          alignItems={'center'}
+          justifyContent={'center'}
+          height={24}
+          width={24}
+        >
           <Box
-            height={18}
-            width={18}
-            borderWidth={2}
-            borderRadius={'sm'}
+            position={'absolute'}
+            top={0}
+            left={0}
+            right={0}
+            bottom={0}
+            backgroundColor={colors.shadowColor}
+            style={styles.shadow}
+            opacity={pressed ? 1 : 0}
+          />
+          <Box
+            height={20}
+            width={20}
             alignItems={'center'}
             justifyContent={'center'}
-            borderColor={
+            backgroundColor={
               value || pressed ? colors.activeColor : colors.borderColor
             }
-            backgroundColor={
-              value ? colors.activeColor : colors.backgroundColor
-            }
+            style={styles.border}
           >
-            <Icon iconName={'Check'} color={colors.iconColor} size="small" />
+            <Box
+              height={16}
+              width={16}
+              alignItems={'center'}
+              justifyContent={'center'}
+              backgroundColor={
+                value ? colors.activeColor : colors.backgroundColor
+              }
+              style={styles.container}
+            >
+              <Icon iconName={'Check'} color={colors.iconColor} size="small" />
+            </Box>
           </Box>
-        </>
+        </Box>
       )}
     </Pressable>
   );
 };
 
-const styles = StyleSheet.create({
-  pressable: {
-    width: 22,
-    height: 22,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const useStyles = (): {
+  pressable: ViewStyle;
+  shadow: ViewStyle;
+  border: ViewStyle;
+  container: ViewStyle;
+} => {
+  const { borderRadii } = useStreamlineTheme();
+
+  const shadowRaddi = borderRadii.sm + 2;
+  const containerRaddi = borderRadii.sm - 2;
+
+  return {
+    pressable: {
+      height: 24,
+      width: 24,
+    },
+    shadow: {
+      borderRadius: shadowRaddi,
+    },
+    border: {
+      borderRadius: borderRadii.sm,
+    },
+    container: {
+      borderRadius: containerRaddi,
+    },
+  };
+};
 
 export default Checkbox;

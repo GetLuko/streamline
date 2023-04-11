ca4a7d41ccdbeaa4a33913b7dd8a83e2e5cb10b5
Hugo Courthias <hugo.courthias@getluko.com>
2023-04-11 16:30:26 +0200

    change margin in checkbox.doc

--- packages/sandbox/src/app/sandbox/docs/checkbox.doc.tsx
+++ packages/sandbox/src/app/sandbox/docs/checkbox.doc.tsx
@@ -5,27 +5,27 @@ export const CheckboxSandbox = () => {
   const [checked, setChecked] = useState(false);
   const [dangerChecked, setDangerChecked] = useState(false);
   return (
-    <Box margin={'md'}>
-      <Box margin={'md'}>
+    <Box margin={'xs'}>
+      <Box margin={'xs'}>
         <Checkbox value={checked} onChange={(v) => setChecked(v)} />
       </Box>
-      <Box margin={'md'}>
+      <Box margin={'xs'}>
         <Checkbox
           appearance="danger"
           value={dangerChecked}
           onChange={(v) => setDangerChecked(v)}
         />
       </Box>
-      <Box margin={'md'}>
+      <Box margin={'xs'}>
         <Checkbox value={true} disabled />
       </Box>
-      <Box margin={'md'}>
+      <Box margin={'xs'}>
         <Checkbox value={false} disabled />
       </Box>
-      <Box margin={'md'}>
+      <Box margin={'xs'}>
         <Checkbox appearance="danger" value={false} disabled />
       </Box>
-      <Box margin={'md'}>
+      <Box margin={'xs'}>
         <Checkbox appearance="danger" value={true} disabled />
       </Box>
     </Box>

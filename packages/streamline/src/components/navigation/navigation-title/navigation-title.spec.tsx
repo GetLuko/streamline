93909fdad1e1beeb3217c37d79f1acc74e7bc4f4
Hugo Courthias <hugo.courthias@getluko.com>
2023-04-17 15:41:58 +0200

    add components to e2e flow

--- packages/streamline/src/components/navigation/navigation-title/navigation-title.spec.tsx
+++ packages/streamline/src/components/navigation/navigation-title/navigation-title.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { fireEvent } from '@testing-library/react-native';
 import NavigationTitle, { NavigationTitleProps } from './navigation-title';
 import { renderWithProvider } from '../../../testing/render-with-provider';
 

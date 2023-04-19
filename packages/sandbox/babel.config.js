793d1eff71cbed403ce7fac870dd8e940e6a5662
ludwig <hello@ludwigvantours.dev>
2023-04-19 17:54:07 +0200

    repair nx + repair bebal

--- packages/sandbox/babel.config.js
+++ packages/sandbox/babel.config.js
@@ -5,6 +5,9 @@ module.exports = function (api) {
     plugins: [
       'transform-inline-environment-variables',
       'react-native-reanimated/plugin',
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      ['@babel/plugin-proposal-private-methods', { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     ],
   };
 };

e1424c0b9ae71ede2f2df6c33f4efaca50135b90
Théo Judas <theo_jd@hotmail.fr>
2023-05-30 11:38:20 +0200

    Default props for Dialog buttons and appearance (#80)
    * Default props for Dialog buttons and appearance
    
    ---------
    
    Co-authored-by: Théo Judas <theo@getluko.com>

--- packages/streamline/src/components/dialog/dialog.types.tsx
+++ packages/streamline/src/components/dialog/dialog.types.tsx
@@ -1,16 +1,20 @@
 import { IconsName } from '../../primitives/icon/icon.types';
 import { ButtonProps } from '../buttons/button/button.types';
 
+interface DialogPrimaryButtonProps extends ButtonProps {
+  appearance?: 'primary' | 'danger' | 'warning';
+}
+
+interface DialogSecondaryButtonProps extends ButtonProps {
+  appearance?: 'neutral' | 'secondary';
+}
+
 export interface DialogProps {
   title: string;
   icon?: IconsName;
   buttons: {
-    orientation: 'horizontal' | 'vertical';
-    primary: ButtonProps & {
-      appearance: 'primary' | 'danger' | 'warning';
-    };
-    secondary?: ButtonProps & {
-      appearance: 'neutral' | 'secondary';
-    };
+    orientation?: 'horizontal' | 'vertical';
+    primary: DialogPrimaryButtonProps;
+    secondary?: DialogSecondaryButtonProps;
   };
 }

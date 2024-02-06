134c837a2f6fe61b184fdf7f39cc7bab56454f37
Hugo Courthias <hugo.courthias@gmail.com>
2024-02-06 13:32:02 +0100

    add types

--- packages/streamline/src/types.ts
+++ packages/streamline/src/types.ts
@@ -8,6 +8,12 @@ export * from './components/buttons/button/button.types';
 export * from './components/cards/card-highlight/card-highlight.types';
 export * from './components/cards/card-feedback/card-feedback.types';
 export * from './components/cards/card-media/card-media.types';
+export * from './components/dialog/dialog.types';
+export * from './components/navigation/navigation-section-title/navigation-section-title.types';
+export * from './components/cards/card-summary/card-summary.types';
+export * from './components/list-items/list-item-group/list-item-group.types';
+export * from './components/list-items/list-item-selectable/list-item-selectable.type';
+export * from './components/cards/card-tip/card-tip.types';
 
 export function isObjKey<T extends object>(key: any, obj: T): key is keyof T {
   return key in obj;

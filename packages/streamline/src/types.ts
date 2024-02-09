236f8be7468ea1cf44fb2f827113faf7f4cd50b6
Hugo Courthias <hugo.courthias@gmail.com>
2024-02-09 10:33:22 +0100

    reordering imports

--- packages/streamline/src/types.ts
+++ packages/streamline/src/types.ts
@@ -2,18 +2,22 @@ import { ButtonProps } from './components/buttons/button/button.types';
 import { TagProps } from './components/tag/tag.types';
 
 export { IconsName } from './primitives/icon/icon.types';
-export * from './components/list-items/list-item-value/list-item-value.types';
-export * from './components/list-items/list-item/list-item.types';
 export * from './components/buttons/button/button.types';
+
 export * from './components/cards/card-highlight/card-highlight.types';
 export * from './components/cards/card-feedback/card-feedback.types';
 export * from './components/cards/card-media/card-media.types';
-export * from './components/dialog/dialog.types';
-export * from './components/navigation/navigation-section-title/navigation-section-title.types';
 export * from './components/cards/card-summary/card-summary.types';
+export * from './components/cards/card-tip/card-tip.types';
+
+export * from './components/dialog/dialog.types';
+
+export * from './components/list-items/list-item/list-item.types';
 export * from './components/list-items/list-item-group/list-item-group.types';
 export * from './components/list-items/list-item-selectable/list-item-selectable.type';
-export * from './components/cards/card-tip/card-tip.types';
+export * from './components/list-items/list-item-value/list-item-value.types';
+
+export * from './components/navigation/navigation-section-title/navigation-section-title.types';
 
 export function isObjKey<T extends object>(key: any, obj: T): key is keyof T {
   return key in obj;

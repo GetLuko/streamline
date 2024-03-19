481e4848962b8724ccd7ed3c936aae9409dfa6f3
Théo Judas <theo@getluko.com>
2024-03-19 09:17:46 +0100

    Reference spacings

--- src/components/list-items/list-items.constants.ts
+++ src/components/list-items/list-items.constants.ts
@@ -1,3 +1,5 @@
+import { spacings } from '../../theme/spacings';
+
 export const BACKGROUND_COLOR = 'PURE_WHITE_1000';
 export const BACKGROUND_PRESSED_COLOR = 'GREY_100';
-export const DIVIDER_VERTICAL_MARGIN = 16;
+export const DIVIDER_VERTICAL_MARGIN = spacings.md;
